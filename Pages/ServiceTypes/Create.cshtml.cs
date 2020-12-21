8bca986a97a448be24644ead783d66f12e8339c1
erenibrahim <63008059+erenibrahim@users.noreply.github.com>
2020-12-21 11:51:15 +0300

    ilk yükleme
    şimdiye kadar ilerletmiş olduğum proje

---
+++ Pages/ServiceTypes/Create.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebProjem.Data;
+using WebProjem.Models;
+
+namespace WebProjem.Pages.ServiceTypes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ServiceType ServiceType { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(ServiceType ServiceType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page(); //istenilen alanlar gönderilmezse uyarý verir
+            }
+
+            _db.ServiceType.Add(ServiceType);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+
+    }
+}
---
+++ Pages/ServiceTypes/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebProjem.Data;
+using WebProjem.Models;
+
+namespace WebProjem.Pages.ServiceTypes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IList<ServiceType> ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            ServiceType = await _db.ServiceType.ToListAsync();
+            return Page();
+        }
+    }
+}
