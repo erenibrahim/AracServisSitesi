76adc949d5a28d4e878bcfcfad1d08a102b310fd
erenibrahim <63008059+erenibrahim@users.noreply.github.com>
2020-12-24 13:52:13 +0300

    sepet modelleri eklendi

---
+++ Pages/Cars/Create.cshtml.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebProjem.Data;
+using WebProjem.Models;
+
+namespace WebProjem.Pages.Cars
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Car Car { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult OnGet(string userId = null)
+        {
+            Car = new Car();
+            if (userId == null)
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                userId = claim.Value;
+            }
+            Car.UserId = userId;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Car.Add(Car);
+            await _db.SaveChangesAsync();
+            StatusMessage = "Araç Eklendi.";
+            return RedirectToPage("Index", new { userId = Car.UserId }); //admin userid ile indexe yönlendirilir
+        }
+    }
+}
